44f52dd897176b46f5fa813ca057cdf2339b6f2f
Mahan Maleki <mahanmaleki950@gmail.com>
2026-02-06 22:43:13 +0330

    fix responsive of header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,12 @@ function Header() {
   return (
     <>
       <header>
-        <div className='header grid grid-cols-4 items-center p-3'>
+        <div className='header grid grid-cols-4 items-center p-3 w-full fixed'>
           <div className="brand col-span-1 flex items-center gap-2">
             <img src="/favicon.ico" alt="tapsi logo" className='w-10 hover:cursor-pointer' />
             <span className='tapsi-bold text-xl hover:cursor-pointer'>Tapsi</span>
           </div>
-          <div className="nav grid-cols-4 col-span-3 gap-5 items-center hidden xl:grid">
+          <div className="nav grid-cols-4 col-span-3 gap-5 items-center hidden lg:grid">
             <div className="menu col-span-3">
               <ul className='text-md flex justify-end'>
                 <li className='text-gray-700 hover:text-orange-500 hover:cursor-pointer me-4 transition-all'>Drivers Registery</li>
@@ -21,10 +21,10 @@ function Header() {
               </ul>
             </div>
             <div className="app col-span-1 items-center">
-              <button className='text-white px-8 py-3 bg-linear-to-l from-orange-600 to-orange-500 rounded-full ms-4 transition-all hover:from-orange-700 hover:to-orange-600 hover:cursor-pointer'>Download Tapsi</button>
+              <button className='text-white px-5 py-3 bg-linear-to-l from-orange-600 to-orange-500 rounded-full ms-4 transition-all hover:from-orange-700 hover:to-orange-600 hover:cursor-pointer'>Download Tapsi</button>
             </div>
           </div>
-          <div className="mobile-nav col-span-3 justify-items-end xl:hidden">
+          <div className="mobile-nav col-span-3 justify-items-end lg:hidden">
             <MobileMenu/>
           </div>
         </div>

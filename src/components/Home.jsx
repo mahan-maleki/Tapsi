3da9cb5b27e1e7225ce8af6b255f8e15ad51f11c
Mahan Maleki <mahanmaleki950@gmail.com>
2026-02-13 11:01:12 +0330

    create Services.jsx component and import & use it to Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from './Hero'
+import Services from './Services'
 
 function Home() {
     return (
         <>
             <main>
                 <Hero />
+                <Services />
             </main>
         </>
     )

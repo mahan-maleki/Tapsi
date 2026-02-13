918835ba82f57c0f4e371b540eaaa8e7f1a53301
Mahan Maleki <mahanmaleki950@gmail.com>
2026-02-13 16:37:59 +0330

    import and use Driver Register Component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from './Hero'
 import Services from './Services'
+import DriverRegister from './DriverRegister'
 
 function Home() {
     return (
@@ -8,6 +9,7 @@ function Home() {
             <main className='z-10'>
                 <Hero />
                 <Services />
+                <DriverRegister />
             </main>
         </>
     )
